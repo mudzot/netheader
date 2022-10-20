fec55a93724c63ad8d2a8d153830138f6eb5ce79
mudzot <mudzot@gmail.com>
2022-10-20 12:24:49 +0700

    Update boost 1.80.0

--- boostnetextract.sh
+++ boostnetextract.sh
@@ -8,7 +8,7 @@ OUTPUT=$2
 
 mkdir -p $OUTPUT/boost
 
-REQUIRED_LIBS="asio assert beast bind config container_hash core date_time detail endian exception intrusive io logic move mp11 mpl numeric/conversion optional predef preprocessor smart_ptr static_assert system throw_exception type_traits utility winapi"
+REQUIRED_LIBS="align asio assert beast bind config container_hash core date_time detail endian exception intrusive io logic move mp11 mpl numeric/conversion optional predef preprocessor smart_ptr static_assert system throw_exception type_traits utility winapi"
 
 for l in $REQUIRED_LIBS; do
   cp -af $REPO_DIR/libs/$l/include/boost/* $OUTPUT/boost/
