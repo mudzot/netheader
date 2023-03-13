2bd80ee5362b15bb1423a74f30b9e32199756cc4
mudzot <mudzot@gmail.com>
2023-03-13 07:40:42 +0700

    Update boost 1.81.0

--- boostnetextract.sh
+++ boostnetextract.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
-#Copy only required header files for boost::beast/boost::asio from boost repository (https://github.com/boostorg/boost.git)
-
-REPO_DIR=$1
-OUTPUT=$2
-: ${REPO_DIR:=.}
-: ${OUTPUT:=networkingheaders}
+                                                                                                                                                                                                
+#Copy only required header files for boost::beast/boost::asio from boost repository (https://github.com/boostorg/boost.git)                                                                                                          
+                                                                                                                                                                                                
+REPO_DIR=$1                                                                                                                                                                                     
+OUTPUT=$2                                                                                                                                                                                       
+                                                                                                                                                                                                
+: ${REPO_DIR:=.}                                                                                                                                                                                    
+: ${OUTPUT:=networkingheaders}                                                                                                                                                                      
 
 mkdir -p $OUTPUT/boost
 
-REQUIRED_LIBS="align asio assert beast bind config container_hash core date_time detail endian exception intrusive io logic move mp11 mpl numeric/conversion optional predef preprocessor smart_ptr static_assert system throw_exception type_traits utility winapi"
+REQUIRED_LIBS="align asio assert beast bind config container_hash core date_time describe detail endian exception intrusive io logic move mp11 mpl numeric/conversion optional predef preprocessor smart_ptr static_assert static_string system throw_exception type_traits utility winapi"
 
 for l in $REQUIRED_LIBS; do
   cp -af $REPO_DIR/libs/$l/include/boost/* $OUTPUT/boost/
